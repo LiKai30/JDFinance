01bc99135f7b68ec24192342d72e99c7c8a32a71
Lee Kai <1106034824@qq.com>
2020-05-08 20:12:27 +0800

    首页部分完成

--- JDFinance/app/js/core/slider.vue
+++ JDFinance/app/js/core/slider.vue
@@ -1,7 +1,7 @@
 <template lang="html">
     <section :class="cname">
         <!-- 需要考虑配置项的传递，也就是传参 -->
-        <swiper :option="options" :not-next-tick="options.noNextTick">
+        <swiper :options="options" :not-next-tick="options.notNextTick">
             <!-- 填充有两种方式：slot和数组 但是使用插槽就必须使用swiper-slide，这样每次都的引入swiper-->
             <swiper-slide v-for="item in items" :key="item.href">
                 <!-- 因为要跳转，所以使用router-link -->
@@ -37,7 +37,7 @@ export default {
                     pagination: {
                         el: ".swiper-pagination",
                     },
-                    noNextTick: false,
+                    notNextTick: false,
                 }
             },
             // 相当于
--- JDFinance/app/js/home/index.vue
+++ JDFinance/app/js/home/index.vue
@@ -3,21 +3,46 @@
     <div>
         <myHeader/>
         <Slider/>
+        <Novice />
+        <Borrow />
+        <Money />
+        <Product />
+        <Life />
+        <myFooter/>
+        <Navbar/>
     </div>
 </template>
 
 <script>
 // vue保留Header标签关键字，所以我们不能直接使用
 import myHeader from "../public/header.vue"
+import myFooter from "../public/footer.vue"
+import Navbar from "../public/navbar.vue"
 import Slider from "./hslider.vue"
+import Novice from "./novice.vue"
+import Borrow from "./borrow.vue"
+import Money from "./money.vue"
+import Product from "./product.vue"
+import Life from "./life.vue"
+
 export default {
     components: {
         myHeader,
         Slider,
+        Novice,
+        Borrow,
+        Money,
+        Product,
+        Life,
+        myFooter,
+        Navbar,
     },
 }
 
 </script>
 
-<style lang="scss" module>
+<style>
+body {
+    background-color: #f5f5f5;
+}
 </style>
